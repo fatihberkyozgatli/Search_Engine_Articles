1c0ba18dc95780c719051d351e66ab9a559d88df
fatihberk <103221845+fatihberkyozgatli@users.noreply.github.com>
2025-10-22 15:41:38 -0500

    Uploadig Files
    Uploading libraries and files from local...

---
+++ build/CMakeFiles/supersearch.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/supersearch.dir/main.cpp.o"
+  "CMakeFiles/supersearch.dir/main.cpp.o.d"
+  "supersearch"
+  "supersearch.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/supersearch.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
